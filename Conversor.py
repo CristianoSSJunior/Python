81fc84b917a16f30d5467c44b9ff86b144b06e09
CristianoSSJunior <cristiano@ezvoice.com.br>
2025-06-16 17:21:25 -0300

    Melhoramento visual, tratamento de erros e melhor organização no código

--- Conversor.py
+++ Conversor.py
@@ -1,3 +1,4 @@
+#Data: 16/06/2025
 from os import system,name
 
 def limpa_tela():
@@ -16,31 +17,34 @@ def r(valor_real,cotacao):
 def d(valor_dolar,cotacao):
      return valor_dolar / cotacao
 
-#Função do conversor
-def main():
-     #Valor da cota do dólar:
-     cotacao = float(5.56)
+def obter_opcao():
+     while True:
+               limpa_tela()
+               print("1- Se você quiser converter do Dólar($) para o Real. \n2- Caso queira converter do Real(R$) para o Dólar.\n")
+               opcao = (input('Digite 1 ou 2: ')).strip()
+               if opcao in ('1','2'):
+                    return int(opcao)
+               print("Opção inválida. Tente novamente.\n")
 
-     deNovo = 'y'
-     while deNovo == 'y':
-          while True:
-               try:
+def converter(opcao):
+     while True:
+          try:
 
-                    limpa_tela()
-                    print("1- Se você quiser converter do Dólar($) para o Real. \n2- Caso queira converter do Real(R$) para o Dólar.\n")
-                    opcao = int(input('Digite 1 ou 2: '))
+               print('\n------------------------|**|----------------------------')
+               #Valor da cota do dólar:
+               cotacao = float(5.5)
 
-                    #Variável para converter dólar para Real
-                    if(opcao == 1):
-                         print("\nConverter Dólar($) -> Real(R$).\nDigite o valor em dólar:\n")
-                         valor_dolar = float(input('$'))
-                         if(valor_dolar >= 2):
-                              print(f'\n${valor_dolar:.2f} dólares valem R${r(valor_dolar,cotacao):.2f} reais.')
-                         elif(valor_dolar <= 1):
-                              print(f'\n${valor_dolar:.2f} dólar vale R${r(valor_dolar,cotacao):.2f} reais.')      
+               #Variável para converter dólar para Real
+               if(opcao == 1):
+                    print("\nConverter Dólar($) -> Real(R$).\nDigite o valor em dólar:\n")
+                    valor_dolar = float(input('$'))
+                    if(valor_dolar >= 2):
+                         print(f'\n${valor_dolar:.2f} dólares valem R${r(valor_dolar,cotacao):.2f} reais.')
+                    elif(valor_dolar <= 1):
+                         print(f'\n${valor_dolar:.2f} dólar vale R${r(valor_dolar,cotacao):.2f} reais.')      
 
                     #Variável para converter Real para dólar.
-                    elif (opcao == 2):
+               elif (opcao == 2):
                          print("\nConverter Real(R$) -> Dólar($).\nDigite o valor em real:\n")
                          valor_real = float(input('R$'))
                          if(valor_real <= 1):
@@ -48,15 +52,30 @@ def main():
                          elif(valor_real >= 2):
                               print(f'\nR${valor_real:.2f} reais valem ${d(valor_real,cotacao):.2f} dólares.')  
 
-                    else:
+               else:
                          print("\nOpção inválida.")
                          continue
 
-                    break
+               break
           
-               except ValueError:
-                    print("\nEntrada inválida. Tente novamente")
-     
+          except ValueError:
+               print("\nEntrada inválida. Tente novamente")
+
+#Função do conversor
+def main():
+
+     deNovo = 'y'
+     while deNovo == 'y':
+
+          #Função para limpar a tela.
+          limpa_tela()
+
+          #Função para obter uma opção.
+          opcao = obter_opcao()
+
+          #Função para converter os valores.
+          converter(opcao)
+
           deNovo = (input("\nQuer converter novamente?(y/n): ")).lower()
           if deNovo not in ('y', 'n'):
                print('Opção inválida. Tente novamente.')
