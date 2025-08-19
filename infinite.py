54a1b3164e91d14c3f2d69b44f6440be34c743e6
CristianoSSJunior <cristiano@ezvoice.com.br>
2025-08-19 17:20:25 -0300

    Added the option to choose how to generate the numbers

--- infinite.py
+++ infinite.py
@@ -11,9 +11,40 @@ def gerar_numeros():
         yield n
         n += 1
 
+#Função que gera números infinitamente, mas não é táo eficiente
+def gerar_numeros_pesado():
+
+    lista_numeros = [x for x in range(9999999)] # Gerador infinito de números
+    print(lista_numeros)
+    return lista_numeros
+
+def option_menu():
+    while True:
+        print("1- Opção 1: Criação otimizada")
+        print("2- Opção 2: Criação pesada")
+        opcao = input("Escolha uma opção (1 ou 2):").strip()
+        if opcao in ('1','2'):
+            return int(opcao)
+        print("Opção inválida. Tente novamente. \n")
+
+def Gerar(opcao):
+    while True:
+        try:
+            if(opcao ==1):
+                return gerar_numeros()
+            else:
+                return gerar_numeros_pesado()
+            break
+        except ValueError:
+            print("Entrada inválida. Tente novamente.")
+
+        
 def main():
     limpa_tela()
-    for numero in gerar_numeros():
+    print("Gerador de números infinitos.\n")
+    opcao = option_menu()
+
+    for numero in Gerar(opcao):
         print(numero)
 
 if __name__ == "__main__":
