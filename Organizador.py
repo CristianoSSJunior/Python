f800a4f42ba32725560bd9b8a96c4c55a65dcf66
CristianoSSJunior <cristiano@ezvoice.com.br>
2025-08-19 15:25:51 -0300

    Sort lists and List comprehension Files

--- Organizador.py
+++ Organizador.py
@@ -1,1 +1,54 @@
 #Criado dia 18/06/2025
+from os import system,name
+
+list=[ 1,4,7,3,9,2,5,8,6,10,12,15,11,13,14,19,18,16,17]
+
+def limpa_tela(self):
+     #windows
+     if name == 'nt':
+           _= system('cls')
+
+     #Mac ou Linux
+     else:
+          _= system('clear')
+
+#Organizador de código
+def organizar_lista(list):
+    #Organiza a lista em ordem crescente
+    list.sort()
+    return list
+
+def organizar_lista_decrescente(list):
+    #Organiza a lista em ordem decrescente
+    list.sort(reverse=True)
+    #Retorna a lista organizada
+    return list
+
+def organizar_lista_aleatoria(list):
+    import random
+    #Organiza a lista de forma aleatória
+    random.shuffle(list)
+    return list
+
+def organizar_lista_pares(list):
+    #Organiza a lista com somente os números pares
+    list[:] = [x for x in list if x % 2 == 0]
+    #Organiza a lista em ordem decrescente
+    list.sort(reverse=True,key=lambda x: x % 2)
+    return list
+
+def main():
+    limpa_tela('self')
+    print("Lista original:", list)
+    organizar_lista(list)
+    print("Lista organizada:", list)
+    organizar_lista_decrescente(list)
+    print("Lista organizada em ordem decrescente:", list)
+    organizar_lista_pares(list)
+    print("Lista organizada com os números pares:", list)
+    organizar_lista_aleatoria(list)
+    print("Lista organizada de forma aleatória:", list)
+
+#Executa a função
+if __name__ == "__main__":
+    main()
---
+++ infinite.py
@@ -0,0 +1,23 @@
+#Criar uma função que crie novos números infinitamente e imprima-os na tela, com a opção de parar a execução.
+import os
+import requests
+
+def limpa_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def main():
+    limpa_tela()
+
+    lista_numeros = [x for x in range(9999999)] # Gerador infinito de números
+    print(lista_numeros)
+    return lista_numeros
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nExecução interrompida pelo usuário.")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+    finally:
+        print("Programa finalizado.")
